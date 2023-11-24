8514aaab5e5d232bcb3a610eb6cafbd47637e513
Fred Guth <fredguth@fredguth.com>
2023-11-24 20:29:28 -0300

    added data

---
+++ data/load.sql
@@ -0,0 +1,6 @@
+COPY ITEM FROM './item.parquet' (FORMAT 'parquet');
+COPY PRODUTO FROM './produto.parquet' (FORMAT 'parquet');
+COPY IBGE FROM './ibge.parquet' (FORMAT 'parquet');
+COPY PJ FROM './pj.parquet' (FORMAT 'parquet');
+COPY COMPRA FROM './compra.parquet' (FORMAT 'parquet');
+COPY MATERIAL FROM './material.parquet' (FORMAT 'parquet');
